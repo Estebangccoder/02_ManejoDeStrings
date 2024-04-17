442bdfaa94bcede8302183134e4fcd2da1bb63a3
estebangccoder <estebangccoder@gmail.com>
2024-04-17 07:46:42 -0500

    inicio del taller

--- src/js/taller.js
+++ src/js/taller.js
@@ -1,3 +1,30 @@
-const number1=Number(promp("Ingrese un numero 1: "))
-const number2=Number(promp("Ingrese un numero 2: "))
+// #Ejercicio 1
+
+const number1 =Number(prompt("Ingrese un numero 1: "))
+const number2 =Number(prompt("Ingrese un numero 2: "))
+const adittion=number1+number2
+
+console.log("The addittion is: ", adittion) 
+
+// #Ejercicio 2
+
+const simpleString1="Hola"
+const simpleString2="Mundo"
+
+console.log("La longitud total de las cadenas es: ", simpleString1.length +simpleString2.length)
+
+// #Ejercicio 3
+
+const stringUsingString1=String("JavaScript")
+const stringUsingString2=String("Ejercicios")
+
+// #Ejercicio 4
+
+const stringUsingNewString1 = new String("Concatenar")
+const stringUsingNewString2 = new String("Strings")
+
+
+
+
+// #Ejercicio 5
 
