93e918e259432d3c73f89eef46b5fe5b6a4c1c6e
estebangccoder <estebangccoder@gmail.com>
2024-04-22 00:39:22 -0500

    mitad del taller de strings

--- src/js/taller.js
+++ src/js/taller.js
@@ -1,30 +1,152 @@
-// #Ejercicio 1
+console.groupCollapsed("Ejercicio 1");
+// Ejercicio 1
 
-const number1 =Number(prompt("Ingrese un numero 1: "))
-const number2 =Number(prompt("Ingrese un numero 2: "))
+let number1 =Number(prompt("Ingrese un numero 1: "))
+let number2 =Number(prompt("Ingrese un numero 2: "))
 const adittion=number1+number2
 
 console.log("The addittion is: ", adittion) 
+console.groupEnd();
 
-// #Ejercicio 2
-
-const simpleString1="Hola"
-const simpleString2="Mundo"
+console.groupCollapsed("Ejercicio 2");
+// Ejercicio 2
+let simpleString1="Hola"
+let simpleString2="Mundo"
 
 console.log("La longitud total de las cadenas es: ", simpleString1.length +simpleString2.length)
 
-// #Ejercicio 3
+console.groupEnd();
 
-const stringUsingString1=String("JavaScript")
-const stringUsingString2=String("Ejercicios")
+console.groupCollapsed("Ejercicio 3");
+// Ejercicio 3
+let stringUsingString1=String("JavaScript")
+let stringUsingString2=String("Ejercicios")
 
-// #Ejercicio 4
+console.groupEnd();
 
-const stringUsingNewString1 = new String("Concatenar")
-const stringUsingNewString2 = new String("Strings")
+console.groupCollapsed("Ejercicio 4");
+// Ejercicio 4
+let stringUsingNewString1 = new String("Concatenar")
+let stringUsingNewString2 = new String("Strings")
 
 
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 5");
+// Ejercicio 5
+
+console.log(simpleString1.indexOf("la"))
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 6");
+// Ejercicio 6
+console.log(simpleString2.includes("ndo"))
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 7");
+// Ejercicio 7
+console.log(simpleString1.concat(" ", simpleString2))
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 8");
+// Ejercicio 8
+console.log(simpleString1 + " " + simpleString2)
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 9");
+// Ejercicio 9
+console.log(`${simpleString1} ${simpleString2}`)
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 10");
+// Ejercicio 10
+let stringWithSpaces1 = " TrimStart" 
+let stringWithSpaces2 = "TrimEnd "
+
+console.log(`${stringWithSpaces1.trimStart()} ${stringWithSpaces2.trimEnd()}`)
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 11");
+// Ejercicio 11
+console.log(simpleString1.replace("o", "i"))
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 12");
+// Ejercicio 12
+console.log(simpleString2.slice(0,3))
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 13");
+// Ejercicio 13
+
+console.log(simpleString2.substring(2))
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 14");
+// Ejercicio 14
+console.log(simpleString1.repeat(2))
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 15");
+// Ejercicio 15
+console.table("Esto es una oración de ejemplo".split(" "))
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 16");
+// Ejercicio 16
+console.info(simpleString2.toUpperCase())
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 17");
+// Ejercicio 17
+console.info(simpleString1.toLowerCase())
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 18");
+// Ejercicio 18
+console.groupEnd();
+
+let booleano = false
+
+console.log(typeof booleano)
+
+console.groupCollapsed("Ejercicio 19");
+// Ejercicio 19
+let arreglo = [a,b,c,d,e,f,g,h,i]
+console.log(arreglo.length)
 
 
-// #Ejercicio 5
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 20");
+// Ejercicio 20
+let vehiculos = {"marca": "Mazda", "referencia": "3-sedan", "modelo": 2006}
+console.log(vehiculos.marca,)
+
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 21");
+// Ejercicio 21
+let nulo = null
+console.log(typeof nulo)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 22");
+// Ejercicio 22
+console.groupEnd();
+
+let undefined
+console.log(typeof undefined)
 
